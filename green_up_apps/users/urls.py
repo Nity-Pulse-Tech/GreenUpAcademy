debfc548171d0a9b8910ae4e3375b2a33659aa23
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-09-04 15:42:03 +0100

    fix added contact

--- green_up_apps/users/urls.py
+++ green_up_apps/users/urls.py
@@ -1,5 +1,6 @@
 from django.db import router
 from django.urls import path
+from green_up_apps.users.views.contact_view import ContactView
 from green_up_apps.users.views.home_views import HomeView
 
 
@@ -7,4 +8,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('contacts', ContactView.as_view(), name='contact'),
+    
 ]
