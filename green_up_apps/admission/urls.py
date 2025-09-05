87cdf476def7ae19b5995c31208ed65b8f73fd14
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-09-05 01:15:25 +0100

    fix design

--- green_up_apps/admission/urls.py
+++ green_up_apps/admission/urls.py
@@ -1,9 +1,10 @@
 from django.db import router
 from django.urls import path
-from green_up_apps.admission.views.hors_ue_views import HorsUeView
+from green_up_apps.admission.views.hors_ue_views import AdmissionView, HorsUeView
 
 app_name = 'admission'
 
 urlpatterns = [
     path('resident-hors-ue/', HorsUeView.as_view(), name='resident_hors_ue'),
+    path('admission/', AdmissionView.as_view(), name='admission'),
 ]
--- green_up_apps/admission/views/hors_ue_views.py
+++ green_up_apps/admission/views/hors_ue_views.py
@@ -4,4 +4,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 class HorsUeView(TemplateView):
-    template_name = "publics/home/admission/resident_hor_ue.html"
+    template_name = "publics/home/admission/resident_hors/resident_hor_ue.html"
+    
+    
+class AdmissionView(TemplateView):
+    template_name = "publics/home/admission/resident_hors/admission_process.html"
