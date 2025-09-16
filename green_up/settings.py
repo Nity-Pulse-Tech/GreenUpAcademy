b3d733373f02fcbd3e2c84b906c6ec5b2bbf6604
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-09-16 22:59:47 +0100

    fix:added debug on the database

--- green_up/settings.py
+++ green_up/settings.py
@@ -91,20 +91,27 @@ WSGI_APPLICATION = 'green_up.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "USER": config("DB_USER"),
-#         "PASSWORD": config("DB_PASSWORD"),
-#         "HOST": config("DB_HOST", default="localhost"),
-#         "PORT": config("DB_PORT", default="5432"),
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if DEBUG:
+    # Local Database (Postgres)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST", default="localhost"),
+            "PORT": config("DB_PORT", default="5432"),
+        }
+    }
+else:
+    
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,  # keeps connections alive
+            ssl_require=True   # enforces SSL in production
+        )
+    }
 
 AUTH_USER_MODEL = 'users.User'
 
